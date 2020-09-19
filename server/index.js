96ecb962ac85cecb48e43eea53b737bfe03ef5b5
Rohan Chakraborty <irohan1999@gmail.com>
2020-09-19 12:00:58 +0530

    -Removed Authentication.json -Removed axios Dependency

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 var express = require("express");
+var faceDetect = require("./faceDetect.js");
 var app = express();
+
 var PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
- console.log("Server running on port "+PORT);
+    console.log("Imoji Server running on port " + PORT);
+    faceDetect.print();
 });
