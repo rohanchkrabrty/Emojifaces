ad096701facb4073ec09d9c6c4ea52cd541e5aa4
Rohan Chakraborty <irohan1999@gmail.com>
2020-09-19 16:51:55 +0530

    + Working Face Recognition + Added Emojis

---
+++ server/faceDetect.js
@@ -0,0 +1,49 @@
+const faceapi = require("face-api.js")
+const mergeImages = require("merge-images")
+const canvas = require("canvas")
+const fs = require("fs")  
+const path = require("path")
+
+// mokey pathing the faceapi canvas
+const { Canvas, Image, ImageData } = canvas  
+faceapi.env.monkeyPatch({ Canvas, Image, ImageData })
+
+// simple utils to save files
+const baseDir = path.resolve(__dirname, './out')  
+function saveFile(fileName, buf) {  
+    if (!fs.existsSync(baseDir)) {
+      fs.mkdirSync(baseDir)
+    }
+    // this is ok for prototyping but using sync methods
+    // is bad practice in NodeJS
+    fs.writeFileSync(path.resolve(baseDir, fileName), buf)
+  }
+function merge(){
+
+}
+async function loadModels(url) {
+    // load weights
+    await faceapi.nets.ssdMobilenetv1.loadFromDisk('./models')
+    await faceapi.nets.faceLandmark68Net.loadFromDisk('./models')
+    await faceapi.nets.faceExpressionNet.loadFromDisk('./models')
+    // load the image
+    const image = await canvas.loadImage(url)
+    //detect face with landmarks and expressions
+    const results = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceExpressions()
+    
+    const box1 = results[0].detection.box;
+    const box2 = results[0].alignedRect.box;
+    const expression = results[0].expressions.asSortedArray()[0].expression;
+    // create a new canvas and draw the detection and landmarks
+    const out = faceapi.createCanvasFromMedia(image)
+    //faceapi.draw.drawDetections(out, results.map(res => res.detection))
+    //faceapi.draw.drawFaceExpressions(out, results)
+    // save the new canvas as image
+    //new faceapi.draw.DrawBox(box1, {lineWidth: 2, boxColor: 'rgba(34, 99, 71, 1)'}).draw(out);
+    //new faceapi.draw.DrawBox(box2, {lineWidth: 2, boxColor: 'rgba(227, 99, 71, 1)'}).draw(out);
+
+    saveFile('detection.jpg', out.toBuffer('image/jpeg'))
+    console.log('done, saved results to out/faceLandmarkDetection.jpg')
+    //console.log(out.toDataURL())
+}
+ module.exports = {loadModels , merge}
--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
-var express = require("express");
-var faceDetect = require("./faceDetect.js");
+const express = require('express')
+const faceDetect = require('./faceDetect.js')
 var app = express();
 
-var PORT = process.env.PORT || 5000;
+var PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-    console.log("Imoji Server running on port " + PORT);
-    faceDetect.print();
+    console.log("Imoji Server running on port " + PORT)
+    //faceDetect.loadModels('https://raw.githubusercontent.com/justadudewhohacks/face-api.js/master/test/images/angry.jpg');
+    faceDetect.merge();
 });
